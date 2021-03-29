dae0cbdcd683b58d43e317414d4dad71304cfe08
COdingaorg <calemasanga@gmail.com>
2021-03-29 14:30:07 +0300

    More practice on js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let name="John",
+/*let name="John",
     admin=name;
     //Interaction.
 let age=prompt('How old are you?',['years']);
@@ -7,5 +7,47 @@ let prof=prompt('What is your proffession?',['work']);
 alert(`Your proffession is ${prof}. Keep it up!`);
 let isAdmin=confirm('Are you the Admin?');
 
-document.getElementById("details").innerHTML= `Your Name is ${admin}. Aged ${age} Years. Your proffession is ${prof}. Keep it up!`;
-//
+document.getElementById("details").innerHTML= `Your Name is ${admin}. Aged ${age} Years. Your proffession is ${prof}. Keep it up!`;*/
+//using conditional statements
+/*let age= prompt("Enter your Age");
+if(age<18){``
+  alert("Enda Kaa na Mama yako");
+}else if(age>18){
+  alert("Bro wwewe ni mzee");
+}else if(age=18){
+  alert("uko tu sawa. Welcome!");
+};
+//using the ? operator
+let proceed=confirm("Do you agree wit our term and conditions?");
+if(proceed==true){
+  alert("Thank you. You can use our site!");
+}else {
+  alert("Sorry you cannot use our site");
+};*/
+/*let age=prompt('Please Enter your Age', 'Number only');
+
+let message=(age<17) ? 'Hello Teenager':
+(age<40) ? 'Hello and Welcome':
+(age<60) ? 'See you later sir or madam':
+ 'HWat an unusual age';
+alert(message);
+//If..else practice.
+let officialName=prompt('Enter the "official" name for javascript');
+if(officialName=="ECMAscript"){
+  alert("Correct answer.");
+}else{
+  alert( `You dont know, it is ECMAscript`);
+};
+
+let num=prompt(`Enter an number...`);
+if(num>0){
+  alert(1);
+}else if(num<0){
+  alert(-1);
+}else if(num==0){
+  alert(0);
+};
+*/
+let num=prompt("Enter an number...", ' ');
+let notif=(num>0) ? 1: (num<0) ? -1: (num==0) ? 0: 'What an unusual number';
+alert(notif);
